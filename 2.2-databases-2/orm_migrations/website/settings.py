b86e4153a3e240fac3fb7121e8ed1b299ddb199a
Dmitry Pyatin <dmitry005@gmail.com>
2021-12-18 19:13:06 +0500

    modification

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -73,7 +73,9 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'django_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
 
