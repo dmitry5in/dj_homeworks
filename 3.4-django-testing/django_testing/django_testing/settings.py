b0d6540e01768bab8fbff3acfe81e3994b107d0c
Dmitry Pyatin <dmitry005@gmail.com>
2022-01-21 14:25:27 +0500

    modification

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'my_database',
     }
 }
 
@@ -124,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,93 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+
+from students.models import Course, Student
+from model_bakery import baker
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_course(client, course_factory):
+    courses = course_factory(_quantity=1)
+    response = client.get(f'/api/v1/courses/{courses[0].id}/')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['name'] == courses[0].name
+
+
+@pytest.mark.django_db
+def test_list_courses(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get('/api/v1/courses/')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == len(courses)
+    for i, m in enumerate(data):
+        assert m['name'] == courses[i].name
+
+
+@pytest.mark.django_db
+def test_filter_by_id(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get(f'/api/v1/courses/{courses[3].id}/')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['id'] == courses[3].id
+
+
+@pytest.mark.django_db
+def test_filter_by_name(client, course_factory):
+    courses = course_factory(_quantity=10)
+    response = client.get(f'/api/v1/courses/', {'name': courses[3].name})
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data[0]['name'] == courses[3].name
+
+
+@pytest.mark.django_db
+def test_create_course(client):
+    response = client.post('/api/v1/courses/', data={'name': 'Python the best'})
+
+    assert response.status_code == 201
+    assert response.data.get('name') == 'Python the best'
+
+
+@pytest.mark.django_db
+def test_update_course(client, course_factory):
+    courses = course_factory(_quantity=1)
+    response = client.patch(f'/api/v1/courses/{courses[0].id}/', data={'name': 'Python'})
+
+    assert response.status_code == 200
+    assert response.data.get('name') == 'Python'
+
+
+@pytest.mark.django_db
+def test_delete_course(client, course_factory):
+    courses = course_factory(_quantity=1)
+    response = client.delete(f'/api/v1/courses/{courses[0].id}/')
+
+    assert response.status_code == 204
